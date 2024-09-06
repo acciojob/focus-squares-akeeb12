18bf5fc39be5f5a25f84a6d1244056a5e019c15e
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-06 12:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll('.square')
+
+squares.forEach(function (square) {
+
+	square.addEventListener('mouseover', function() {
+		squares.forEach(function(otherSquare) {
+			otherSquare.style.backgroundColor = '#6F4E37';
+		});
+		this.style.backgroundColor = '#E6E6FA';
+	});
+	square.addEventListener('mouseout',function() {
+		squares.forEach(function (otherSquare) {
+			otherSquare.style.backgroundColor = '#E6E6FA';
+			
+		});
+	});
+});
